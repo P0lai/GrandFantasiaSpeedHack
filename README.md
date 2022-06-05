746fa0c2137a9ac1451fccf36bf05bd7f3e57799
P0lai <polaigfpt@gmail.com>
2022-06-05 20:19:53 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# README
+
+Este programa foi feito com o propósito 4fun, você não irá obter nenhuma vantagem sobre outros players,
+apenas na sua visão você estará rapido, e no lado do servidor e na visão de outros players você continuará com sua
+velocidade normal :).
+
+# Como usar?
+
+Abra o jogo.
+Dê alguns passos.
+Abra o Speed Hack COMO ADMINISTRADOR.
+Insirá o valor.
+
+Voilà :D
---
+++ speed hack.py
@@ -0,0 +1,50 @@
+from pymem import *
+from pymem.process import *
+from os import system 
+
+system("cls||clear")
+
+try:
+    mem = Pymem("GrandFantasia.exe")
+except:
+    print("\033[1;91m[*]The process 'GrandFantasia.exe' not found.\n")
+    system("pause")
+    exit()
+
+module = module_from_name(mem.process_handle, "GrandFantasia.exe").lpBaseOfDll
+offsets = [0xD0, 0x0, 0x0, 0x40, 0x10, 0x8, 0x80]
+
+def getPointerAddr(base, offsets):
+    addr = mem.read_int(base)
+
+    for offset in offsets:
+        if offset != offsets[-1]:
+            addr = mem.read_int(addr + offset)
+    
+    addr = addr + offsets[-1]
+
+    return addr
+
+def main():
+    while True:
+        print("\033[1;96m" + """
+        [*] GRAND FANTASIA SPEED HACK - 04/06/2022
+                    by: Polai\n""")
+
+        try:
+            speed_value = float((input("\033[1;97m" + "Value: ")))
+
+            if speed_value > 100 or speed_value < 1:
+                system("cls||clear")
+                print("\n" + "\033[1;91m" + "[-] The minimum speed value is 1 and maximum 100.")
+                main()
+        except:
+            system("cls||clear")
+            print("\n" + "\033[1;91m" + "[-] Only numbers.")    
+            main()
+        mem.write_float(getPointerAddr(module + 0x0096B6CC, offsets), speed_value)
+
+        system("cls||clear")
+
+        print("\n" + "\033[1;92m" + "[+] Injected.\n")
+main()
